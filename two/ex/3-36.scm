7467b49a4dc05a2ff45e02c0039a353addcc94b3
whchao <feijihqda@126.com>
2016-08-19 13:48:06 +0800

    git log

--- two/ex/3-36.scm
+++ two/ex/3-36.scm
@@ -16,4 +16,10 @@
         (cons (proc (car items))
               (map proc (cdr items)))))
 
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))              
+
 (define x (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))            
